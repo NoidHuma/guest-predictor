d46869c18d7f6b0fb60f32183f0f23cc21eff4ad
NoidHuma <noidhuma0@gmail.com>
2026-09-24 22:13:20 +0300

    refactor error hadling

--- predict.py
+++ predict.py
@@ -176,4 +176,8 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except ValueError as error:
+        print(f"Ошибка: {error}")
+        raise SystemExit(1)
